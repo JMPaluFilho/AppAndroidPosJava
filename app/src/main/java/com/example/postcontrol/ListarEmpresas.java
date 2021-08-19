ac53c567a102acca98bd57a05703c4f948a4812b
JMPaluFilho <46405119+JMPaluFilho@users.noreply.github.com>
2021-08-19 01:01:17 -0300

    Ajustes para entrega da semana quatro

--- app/src/main/java/com/example/postcontrol/ListarEmpresas.java
+++ app/src/main/java/com/example/postcontrol/ListarEmpresas.java
@@ -1,56 +1,163 @@
 package com.example.postcontrol;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.view.ActionMode;
 
-import com.example.postcontrol.adapter.EmpresaAdapter;
 import com.example.postcontrol.entity.Empresa;
 
 import java.util.ArrayList;
 
 public class ListarEmpresas extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private static final ArrayList<Empresa> empresas = new ArrayList<>();
+    private ListView listView;
+    private ArrayAdapter<Empresa> listAdapter;
+    private ActionMode actionMode;
+    private View viewSelecionada;
+    private int posicaoSelecionada = -1;
+    private ArrayList<Empresa> empresas;
+
+
+    private final ActionMode.Callback menuActionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.empresa_opcoes, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menuItemEditar:
+                    editarEmpresa();
+                    mode.finish();
+                    return true;
+                case R.id.menuItemExcluir:
+                    excluirEmpresa();
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            if (viewSelecionada != null) {
+                viewSelecionada.setBackgroundColor(Color.TRANSPARENT);
+            }
+            actionMode = null;
+            viewSelecionada = null;
+            listView.setEnabled(true);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_empresas);
-        recyclerView = findViewById(R.id.recycler_view_main);
+        listView = findViewById(R.id.list_view_main);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            posicaoSelecionada = position;
+            editarEmpresa();
+        });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            if (actionMode != null) {
+                return false;
+            }
+            posicaoSelecionada = position;
+            view.setBackgroundColor(Color.LTGRAY);
+            viewSelecionada = view;
+            listView.setEnabled(false);
+            actionMode = startSupportActionMode(menuActionModeCallback);
+            return true;
+        });
+        popularLista();
     }
 
-    public void adicionarEmpresa(View view) {
-        Intent intent = new Intent(this, CadastrarEmpresa.class);
-        intent.putExtra(getString(R.string.result), 1);
-        startActivityForResult(intent, 1);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.listar_empresas_opcoes, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.menuItemAdicionar:
+                CadastrarEmpresa.novaEmpresa(this);
+                return true;
+            case R.id.menuItemSobre:
+                intent = new Intent(this, MostrarAutoria.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (resultCode == Activity.RESULT_OK) {
             Bundle bundle = data.getExtras();
-            if (bundle != null) {
-                Empresa empresa = (Empresa) bundle.getSerializable(getString(R.string.empresa));
-                popularLista(empresa);
+            Empresa empresa = (Empresa) bundle.getSerializable(getString(R.string.empresa));
+
+            if (requestCode == CadastrarEmpresa.EDITAR) {
+                Empresa empresaExistente = empresas.get(posicaoSelecionada);
+                empresaExistente.setNomeEmpresa(empresa.getNomeEmpresa());
+                empresaExistente.setDtInicioContrato(empresa.getDtInicioContrato());
+                empresaExistente.setValorContrato(empresa.getValorContrato());
+                empresaExistente.setCnpj(empresa.getCnpj());
+                empresaExistente.setServicoContratado(empresa.getServicoContratado());
+                empresaExistente.setFrequenciaSemanal(empresa.getFrequenciaSemanal());
+                empresaExistente.setContratoAtivo(empresa.getContratoAtivo());
+                posicaoSelecionada = -1;
+            } else {
+                empresas.add(empresa);
             }
+            listAdapter.notifyDataSetChanged();
         }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void mostrarInfos(View view) {
-        Intent intent = new Intent(this, MostrarAutoria.class);
-        startActivity(intent);
+    private void popularLista() {
+        empresas = new ArrayList<>();
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, empresas);
+        listView.setAdapter(listAdapter);
     }
 
-    private void popularLista(Empresa empresa) {
-        empresas.add(empresa);
-        EmpresaAdapter empresaAdapter = new EmpresaAdapter(empresas);
-        recyclerView.setAdapter(empresaAdapter);
+    private void excluirEmpresa() {
+        empresas.remove(posicaoSelecionada);
+        listAdapter.notifyDataSetChanged();
+    }
+
+    private void editarEmpresa() {
+        Empresa empresa = empresas.get(posicaoSelecionada);
+        CadastrarEmpresa.editarEmpresa(this, empresa);
     }
 }
--- app/src/main/java/com/example/postcontrol/MostrarAutoria.java
+++ app/src/main/java/com/example/postcontrol/MostrarAutoria.java
@@ -1,7 +1,12 @@
 package com.example.postcontrol;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MostrarAutoria extends AppCompatActivity {
@@ -10,5 +15,26 @@ public class MostrarAutoria extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_autoria);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
